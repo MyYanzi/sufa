4027da7a9abe71adab6db5e6bf3f6f5d6c8127b9
MyYanzi <572963943@qq.com>
2019-04-15 22:42:44 +0800

    Helloworld修改

--- src/dongTai/Helloworld.java
+++ src/dongTai/Helloworld.java
@@ -2,6 +2,6 @@ package dongTai;
 
 public class Helloworld {
 	 public static void main(String[] args) {
-	        System.out.println("Hello World");
+	        System.out.println("Hello World修改版");
 	    }
 }
